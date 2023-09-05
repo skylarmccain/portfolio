38be3543785fbd623868f3f72fbcba70f2f5f2df
skylarmccain <skylarmccain@ufl.edu>
2023-09-05 09:17:52 -0400

    Added daisyui dependency and configured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to Home Page</h1>
+<div>
+    <h1 class="text-3xl font-bold underline">
+        Welcome to Home Page
+    </h1>
+    <button class="btn btn-primary">Button</button>
+</div>
 
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
