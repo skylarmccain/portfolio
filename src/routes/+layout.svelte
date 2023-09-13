646af7297328c0c52d3e362e3f212632ece1438a
skylarmccain <skylarmccain@ufl.edu>
2023-09-13 08:38:23 -0400

    layout.js and build commands

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script>
     /** @type {import('./$types').LayoutData} */
     import "../app.css";
-    export let data;
-    export const prerender = true;
-    import { Hamburger } from "svelte-hamburgers";
-    import Sidebar from "../lib/components/Sidebar.svelte";
-
-    let open = false;
 </script>
 
 
